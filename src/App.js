009edfc9049738737c8ad6fb4e50b4786ce49f5a
Tom Fielder <tom.fielder97@gmail.com>
2022-04-16 22:20:12 +0100

    Change image, add click coordinate detection

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import { HashRouter, Routes, Route } from "react-router-dom";
-import './App.css';
-import demoImage from "./assets/images/allmon.jpg"
+import Game from "./components/game.js"
 
 function App() {
+  
   return (
     <div className="App">
-      Hello World
-      <img src={demoImage} alt="Many pokemon in a Where's Wally style" className="game-image"></img>
+      <Game />
     </div>
   );
 }
